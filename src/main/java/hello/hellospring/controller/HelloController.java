4fd634c75603ef56b3edde20843593806c46d793
nanami-tomoe <npnp123npnp123@gmail.com>
2023-10-04 22:52:13 +0900

    Feat: hello-template.html 추가
    - mvc 추가

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,9 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model; // Model 위에 커서 올리고 alt + Enter 누르고 import
+import org.springframework.ui.Model; // Model 위에 커서 올리고 option + Enter 누르고 import
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // 노테이션
 public class HelloController {
@@ -12,5 +13,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!"); // model은 MVC(Model View Controller)
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template"; // hello-template 로 간다
     }
 }
